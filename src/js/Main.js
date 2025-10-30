d661698d3bdd36303ca0878b3bf7ad5b2c7a1e7f
Johannes <j.gramm95@googlemail.com>
2025-10-30 08:46:00 +0100

    erste uebung - layout und liste

--- src/js/Main.js
+++ src/js/Main.js
@@ -3,30 +3,28 @@
  * by the framework at runtime and "exports" them to the framework
  * setting the mwf.app.components object
  */
-import {mwf} from "vfh-iam-mwf-base";
-import {GenericDialogTemplateViewController} from "vfh-iam-mwf-base";
+import { GenericDialogTemplateViewController, mwf } from "vfh-iam-mwf-base";
 
 /* application libraries: the main application class */
 import MyApplication from "./MyApplication.js";
 /* application libraries: model */
-import * as entities from "./model/MyEntities.js";
 /* application libraries: view controllers */
+import ListViewController from "./controller/ListViewController.js";
 import MyInitialViewController from "./controller/MyInitialViewController.js";
 // TODO-REPEATED: import any further view controllers here
 
 // we export the framework modules required by the application and the application modules required by the framework
 mwf.app.components = {
-    /* we need to provide the generic view controller as part of the classes that will be dynamically instantiated by the framework */
-    GenericDialogTemplateViewController,
-    /* application modules */
-    MyApplication,
-    MyInitialViewController
-    // TODO-REPEATED: export any further view controllers here
-}
+  /* we need to provide the generic view controller as part of the classes that will be dynamically instantiated by the framework */
+  GenericDialogTemplateViewController,
+  /* application modules */
+  MyApplication,
+  MyInitialViewController,
+  ListViewController,
+  // TODO-REPEATED: export any further view controllers here
+};
 
 // then start the application
 window.onload = () => {
-    mwf.onloadApplication();
-}
-
-
+  mwf.onloadApplication();
+};
--- src/js/model/MyEntities.js
+++ src/js/model/MyEntities.js
@@ -4,23 +4,32 @@
  * this skript defines the data types used by the application and the model operations for handling instances of the latter
  */
 
-import {mwfUtils} from "vfh-iam-mwf-base";
-import {EntityManager} from "vfh-iam-mwf-base";
+import { EntityManager } from "vfh-iam-mwf-base";
 
 /*************
  * example entity
  *************/
 
 export class MyEntity extends EntityManager.Entity {
+  // TODO-REPEATED: declare entity instance attributes
 
-    // TODO-REPEATED: declare entity instance attributes
-
-    constructor() {
-        super();
-    }
-
+  constructor() {
+    super();
+  }
 }
 
 // TODO-REPEATED: add new entity class declarations here
 
+export class MediaItem extends EntityManager.Entity {
+  title;
+  src;
+  contentType;
+  added = Date.now();
+  description = "";
 
+  constructor(title, src) {
+    super();
+    this.title = title;
+    this.src = src;
+  }
+}
